a040b2e98da8f90f8f1c412aad73208671a7b16e
PRATHAM SAHANI <106865923+PrathamSahani@users.noreply.github.com>
2023-06-18 20:03:14 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Arya's Long String/aryas-long-string.java
@@ -0,0 +1,45 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class GFG{
+	public static void main (String[] args) throws IOException{
+    	BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+    	while((t--)!=0){
+    	    String s = read.readLine();
+    	    String input_line[] = read.readLine().trim().split("\\s+");
+    	    int k=Integer.parseInt(input_line[0]);
+            int n=Integer.parseInt(input_line[1]);
+            char c=input_line[2].charAt(0);
+    	    Solution obj = new Solution();
+    	    System.out.println(obj.fun(s, k, n, c));
+    	}
+	}
+}
+// } Driver Code Ends
+
+// Easy Java Solution
+
+class Solution{
+    public static int fun(String s, int k, int n, char c)
+    {
+        // Your code here
+        int count=0;
+        for(int i=0;i<s.length();i++){
+            if(s.charAt(i)==c){
+                count++;
+            }
+        }
+        count=count*(n/s.length());
+        for(int i=0;i<n%s.length();i++){
+            if(s.charAt(i)==c){
+                count++;
+            }
+        }
+        return count;
+    }
+}
