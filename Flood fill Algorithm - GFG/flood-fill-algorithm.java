dbe6a96a89bf278c659acc6505d5fc7a8699b290
PRATHAM SAHANI <106865923+PrathamSahani@users.noreply.github.com>
2023-06-28 10:42:18 +0530

    Added solution - LeetHub

---
+++ Flood fill Algorithm - GFG/flood-fill-algorithm.java
@@ -0,0 +1,69 @@
+//{ Driver Code Starts
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class GFG
+{
+    public static void main(String[] args) throws IOException
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine().trim());
+        while(T-->0)
+        {
+            String[] S1 = br.readLine().trim().split(" ");
+            int n = Integer.parseInt(S1[0]);
+            int m = Integer.parseInt(S1[1]);
+            int[][] image =  new int[n][m];
+            for(int i = 0; i < n; i++){
+                String[] S2 = br.readLine().trim().split(" ");
+                for(int j = 0; j < m; j++)
+                    image[i][j] = Integer.parseInt(S2[j]);
+            }
+            String[] S3 = br.readLine().trim().split(" ");
+            int sr = Integer.parseInt(S3[0]);
+            int sc = Integer.parseInt(S3[1]);
+            int newColor = Integer.parseInt(S3[2]);
+            Solution obj = new Solution();
+            int[][] ans = obj.floodFill(image, sr, sc, newColor);
+            for(int i = 0; i < ans.length; i++){
+                for(int j = 0; j < ans[i].length; j++)
+                    System.out.print(ans[i][j] + " ");
+                System.out.println();
+            }
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+class Solution
+{
+    public static void fillArray(int img[][], int row, int col, int prev, int newColor){
+        if(row<0 || row>=img.length || col<0 || col>=img[0].length){
+            return;
+        }
+        if(img[row][col] == newColor){
+            return;
+        }
+        if(img[row][col]!=prev){
+            return ;
+        }
+        if(img[row][col]==prev){
+            img[row][col] = newColor;
+        }
+        fillArray(img, row+1, col, prev, newColor);
+        fillArray(img, row-1, col, prev, newColor);
+        fillArray(img, row, col-1, prev, newColor);
+        fillArray(img, row, col+1, prev, newColor);
+        
+    }
+    public int[][] floodFill(int[][] image, int sr, int sc, int newColor)
+    {
+        // Code here 
+        int arr[][] = image;
+        int prev = image[sr][sc];
+        fillArray(arr, sr, sc, prev, newColor);
+        return arr;
+    }
+}
