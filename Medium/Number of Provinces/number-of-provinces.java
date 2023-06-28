af253d06f00adb6bd80f4f4f69833d1715e1ebdf
PRATHAM SAHANI <106865923+PrathamSahani@users.noreply.github.com>
2023-06-28 09:27:46 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Number of Provinces/number-of-provinces.java
@@ -0,0 +1,54 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String args[]) throws IOException {
+        BufferedReader read =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while (t-- > 0) {
+            int V = Integer.parseInt(read.readLine());
+            
+            ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+            
+            for(int i=0; i<V; i++)
+            {
+                String S[] = read.readLine().split(" ");
+                ArrayList<Integer> temp = new ArrayList<>();
+                for(int j=0; j<V; j++)
+                    temp.add(Integer.parseInt(S[j]));
+                adj.add(temp);
+            }
+
+            Solution ob = new Solution();
+            System.out.println(ob.numProvinces(adj,V));
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution {
+    public  static void dfs(int j, boolean vis[], ArrayList<ArrayList<Integer>> adj){
+        vis[j] = true;
+        for(int i=0; i<adj.size(); i++){
+            if(vis[i]==false && adj.get(j).get(i)==1){
+                dfs(i,  vis, adj);
+            }
+        }
+    }
+    static int numProvinces(ArrayList<ArrayList<Integer>> adj, int V) {
+        // code here
+        boolean vis[] = new boolean[V];
+        int c=0;
+        for(int i=0; i<V; i++){
+            if(vis[i]==false){
+                dfs(i,vis, adj);
+                c++;
+            }
+    }
+    return c;}
+};
