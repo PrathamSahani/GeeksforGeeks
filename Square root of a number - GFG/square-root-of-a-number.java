b4df5bc949d85d7f4eca3a60099fe54c70c12fe1
PRATHAM SAHANI <106865923+PrathamSahani@users.noreply.github.com>
2023-06-29 21:28:18 +0530

    Added solution - LeetHub

--- Square root of a number - GFG/square-root-of-a-number.java
+++ Square root of a number - GFG/square-root-of-a-number.java
@@ -34,13 +34,13 @@ class Solution
 	  while(low<=high){
 	      long mid = (low+high)/2;
 	      if(mid*mid<=x){
-	          ans = mid;
+	        
 	          low = mid+1;
 	      }else{
 	          high = mid-1;
 	      }
 	      
 	  }
-	  return ans;
+	  return high;
 	 }
 }
