12a199da8c956e4a4c4645d79cec431530e37a2a
PRATHAM SAHANI <106865923+PrathamSahani@users.noreply.github.com>
2023-06-29 21:16:47 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Square root of a number/square-root-of-a-number.java
@@ -0,0 +1,42 @@
+//{ Driver Code Starts
+import java.util.Scanner;
+
+class SquartRoot
+{
+	public static void main(String args[])
+	{
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		while(t>0)
+		{
+			long a = sc.nextInt();
+			Solution obj = new Solution();
+			System.out.println(obj.floorSqrt(a));
+		t--;
+		}
+	}
+}
+// } Driver Code Ends
+
+
+/*You are required to complete
+this function*/
+
+// Function to find square root
+// x: element to find square root
+class Solution
+{
+     long floorSqrt(long x)
+	 {
+		// Your code here
+		long ans =1;
+		for(int i=1; i<=x; i++){
+		    if(i*i<=x){
+		        ans =i;
+		    }else{
+		        break;
+		    }
+		}
+		return ans;
+	 }
+}
