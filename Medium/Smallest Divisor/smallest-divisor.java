c2fed1153e9a1d81934ca2bb15420567f762c014
PRATHAM SAHANI <106865923+PrathamSahani@users.noreply.github.com>
2023-06-30 00:35:25 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Smallest Divisor/smallest-divisor.java
@@ -0,0 +1,62 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+class GFG {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int t;
+        t = sc.nextInt();
+        while (t-- > 0) {
+
+            int n = sc.nextInt();
+            int K;
+            K = sc.nextInt();
+            int[] nums = new int[n];
+            for (int i = 0; i < n; i++) nums[i] = sc.nextInt();
+
+            Solution obj = new Solution();
+            int res = obj.smallestDivisor(nums, K);
+
+            System.out.println(res);
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    
+    public static int findMax(int[] arr){
+        int ans = Integer.MIN_VALUE;
+        for(int i=0; i<arr.length; i++){
+            ans  = Math.max(ans, arr[i]);
+        }
+        return ans;
+    }
+    public static int solve(int arr[], int hour){
+        int ans = 0;
+        for(int i=0; i<arr.length; i++){
+            ans+= Math.ceil(((double)arr[i])/(double)hour);
+        }
+        return ans;
+    }
+    public static int smallestDivisor(int[] nums, int K) {
+        int low = 1, high = findMax(nums);
+        while(low<=high){
+            int mid = (low+high)/2;
+            int ans= solve(nums, mid);
+            if(ans<=K){
+                high = mid-1;
+            }else{
+                low = mid+1;
+            }
+        }
+        return low;
+        
+    }
+}
