72b22d0e185ea67bc2ce541e274d17d80853d285
PRATHAM SAHANI <106865923+PrathamSahani@users.noreply.github.com>
2023-06-30 22:30:29 +0530

    Added solution - LeetHub

--- Bubble Sort - GFG/bubble-sort.java
+++ Bubble Sort - GFG/bubble-sort.java
@@ -14,16 +14,30 @@ class Solution
 	public static void bubbleSort(int arr[], int n)
     {
         //code here
-         for(int i=n-1; i>=0; i--){
-             for(int j=0; j<=i-1; j++){
-                 if(arr[j]>arr[j+1]){
-                     int temp = arr[j+1];
-                     arr[j+1] = arr[j];
-                     arr[j] = temp;
-                 }
-             }
-         }
-         
+        // bubble sort
+        //  for(int i=n-1; i>=0; i--){
+        //      for(int j=0; j<=i-1; j++){
+        //          if(arr[j]>arr[j+1]){
+        //              int temp = arr[j+1];
+        //              arr[j+1] = arr[j];
+        //              arr[j] = temp;
+        //          }
+        //      }
+        //  }
+        
+        se
+        
+          for(int i=0; i<=n-2; i++){
+              int mini = i;
+              for(int j=i; j<=n-1; j++){
+                  if(arr[j]<arr[mini]){
+                  mini = j;
+                  }
+              }
+              int temp = arr[mini];
+              arr[mini] = arr[i];
+              arr[i] = temp;
+          }
         
     }
 }
