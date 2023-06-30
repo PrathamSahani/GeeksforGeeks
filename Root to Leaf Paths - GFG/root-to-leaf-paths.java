de5ae581fa3afe7e0ffd57b0b754be26f57e91f4
PRATHAM SAHANI <106865923+PrathamSahani@users.noreply.github.com>
2023-06-30 15:14:32 +0530

    Added solution - LeetHub

--- Root to Leaf Paths - GFG/root-to-leaf-paths.java
+++ Root to Leaf Paths - GFG/root-to-leaf-paths.java
@@ -134,20 +134,20 @@ class Node{
 
 */
 class Tree{
-    public void getPath(Node root, ArrayList<ArrayList<Integer>> al, ArrayList<Integer> temp){
-        temp.add(root.data);
-        if(root.left==null && root.right==null){
-            al.add(new ArrayList(temp));
-            temp.remove(temp.size()-1);
-            return ;
-        }
-        if(root.left!=null){
-            getPath(root.left, al, temp);
-        }if(root.right!=null){
-            getPath(root.right, al, temp);
-        }
-        temp.remove(temp.size()-1);
-    }
+ public void getPath(Node root, ArrayList<ArrayList<Integer>> al, ArrayList<Integer> temp){
+     temp.add(root.data);
+     if(root.left==null && root.right==null){
+         al.add(new ArrayList<>(temp));
+         temp.remove(temp.size()-1);
+         return ;
+     }
+     if(root.left!=null){
+         getPath(root.left, al, temp);
+     }if(root.right!=null){
+         getPath(root.right, al, temp);
+     }
+     temp.remove(temp.size()-1);
+ }
     public ArrayList<ArrayList<Integer>> Paths(Node root){
         // Code here
         ArrayList<ArrayList<Integer>> al = new ArrayList<>();
