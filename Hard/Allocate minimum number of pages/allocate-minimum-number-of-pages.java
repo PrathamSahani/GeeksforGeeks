019de89cf9b76810973a58ee3081301081ed7246
PRATHAM SAHANI <106865923+PrathamSahani@users.noreply.github.com>
2023-07-01 13:58:06 +0530

    Added Solution - GfG to GitHub

---
+++ Hard/Allocate minimum number of pages/allocate-minimum-number-of-pages.java
@@ -0,0 +1,91 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+/*package whatever //do not write package name here */
+
+import java.io.*;
+import java.util.*;
+class GFG {
+	public static void main (String[] args) {
+		Scanner sc=new Scanner(System.in);
+		
+		int t=sc.nextInt();
+		
+		while(t-->0)
+		{
+		    int n=sc.nextInt();
+		    int a[]=new int[n];
+		    
+		    for(int i=0;i<n;i++)
+		    {
+		        a[i]=sc.nextInt();
+		    }
+		    int m=sc.nextInt();
+		    Solution ob = new Solution();
+		    System.out.println(ob.findPages(a,n,m));
+		}
+	}
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution 
+{
+    //Function to find minimum number of pages.
+    public static boolean allocation(int [] A, int student, int target){
+        int pages=0,  total=1;
+        for(int i=0; i<A.length; i++){
+            if(A[i]>target){
+                return false;
+            }
+            if(pages+A[i]>target){
+                total+=1;
+                pages= A[i];
+            }else{
+                pages+=A[i];
+            }
+        }
+        if(total>student)return false;
+        return true;
+    }
+    public static int findPages(int[]A,int N,int M)
+    {
+        //Your code here
+        if(M>N)return -1;
+        int low = A[0];int high = 0;
+        for(int i=0; i<N; i++){
+            high+=A[i];
+        }
+        int res =-1;
+        while(low<=high){
+            int mid = low+(high-low)/2;
+            if(allocation(A, M, mid)){
+                res = mid;
+                high = mid-1;
+            }else{
+                low = mid+1;
+            }
+        }
+        return res;
+    }
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
