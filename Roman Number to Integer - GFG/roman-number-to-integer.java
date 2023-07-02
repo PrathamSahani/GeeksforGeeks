220c9850d8ad8601f638fd3a78897c7e59ab1571
PRATHAM SAHANI <106865923+PrathamSahani@users.noreply.github.com>
2023-07-02 23:37:34 +0530

    Added solution - LeetHub

--- Roman Number to Integer - GFG/roman-number-to-integer.java
+++ Roman Number to Integer - GFG/roman-number-to-integer.java
@@ -26,24 +26,23 @@ class Solution {
     // Finds decimal value of a given roman numeral
     public int romanToDecimal(String str) {
         // code here
-        int ans =0, num =0;
-        for(int i=str.length()-1; i>=0; i--){
-            switch(str.charAt(i)){
-                case 'I':num =1;break;
-                case 'V': num= 5; break;
-                case 'X':num =10; break;
-                case 'L':num =50; break;
-                case 'C':num =100; break;
-                case 'D':num =500; break;
-                case 'M':num =1000; break;
-            }
-            if(4*num<ans)
-            ans-=num;
-            else
-            ans+=num;
-        }
-        return ans;
-  
-        
+         int ans=0, num =0;
+         for(int i=str.length()-1; i>=0; i--){
+             switch(str.charAt(i)){
+                 case 'I':num=1;break;
+                 case 'V':num=5; break;
+                 case 'X':num=10;break;
+                 case 'L':num=50;break;
+                 case 'C':num=100;break;
+                 case 'D':num=500;break;
+                 case 'M':num=1000;break; 
+             }
+             if(4*num<ans)
+              ans-=num;
+              else
+              ans+=num;
+              
+         }
+         return ans;
     }
 }
