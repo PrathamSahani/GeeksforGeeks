981d0e0ea89cb913bb93e224abfae80e2b6f7477
PRATHAM SAHANI <106865923+PrathamSahani@users.noreply.github.com>
2023-07-05 09:58:59 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Subset Sum Problem/subset-sum-problem.java
@@ -0,0 +1,60 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+class GFG
+{
+    public static void main(String args[])throws IOException
+    {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while(t-- > 0)
+        {
+            int N = Integer.parseInt(read.readLine());
+            String input_line[] = read.readLine().trim().split("\\s+");
+            int arr[]= new int[N];
+            for(int i = 0; i < N; i++)
+                arr[i] = Integer.parseInt(input_line[i]);
+            int sum = Integer.parseInt(read.readLine());
+
+            Solution ob = new Solution();
+            if(ob.isSubsetSum(N, arr, sum))
+            System.out.println(1);
+            else
+            System.out.println(0);
+
+            
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution{
+    static boolean f(int arr[], int ind, int target, int[][] dp){
+        if(target==0)return true;
+        if(ind==0)return arr[0]==target;
+        if(dp[ind][target]!=-1)return dp[ind][target]==0?false:true;
+         
+         boolean notake = f(arr, ind-1, target , dp);
+         boolean take = false;
+         if(target>=arr[ind]){
+             take = f(arr, ind-1, target-arr[ind], dp);
+         }
+         dp[ind][target] =notake||take?1:0;
+         return notake||take;
+    }
+
+
+    static Boolean isSubsetSum(int N, int arr[], int sum){
+        // code here
+        int dp[][] = new int[N][sum+1];
+        for(int row[]: dp){
+            Arrays.fill(row, -1);
+        }
+        return f(arr, N-1, sum , dp);
+    }
+}
