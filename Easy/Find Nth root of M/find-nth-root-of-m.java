8313058e7dd48877ce183182b860437fced63c27
PRATHAM SAHANI <106865923+PrathamSahani@users.noreply.github.com>
2023-07-07 08:40:41 +0530

    Updated Solution - GfG to GitHub

--- Easy/Find Nth root of M/find-nth-root-of-m.java
+++ Easy/Find Nth root of M/find-nth-root-of-m.java
@@ -30,24 +30,30 @@ class GFG
 
 class Solution
 {
-    public static int solve(int mid, int n, int m){
+    public static int func(int mid, int n, int m){
         long ans =1;
         for(int i=1; i<=n; i++){
             ans = ans*mid;
             if(ans>m)return 2;
-        }if(ans==m)return 1;
+            
+        }
+        if(ans ==m)return 1;
         return 0;
     }
     public int NthRoot(int n, int m)
     {
-        // code here]
-        int low =1, high = m;
-        while(low<=high){
-            int mid = low+(high-low)/2;
-            if(solve(mid, n, m)==1)return mid;
-            else if(solve(mid, n, m)==0)low = mid+1;
-            else
-            high = mid-1;
+        // code here
+        int low = 1, high = m;
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            int midN = func(mid, n, m);
+            if (midN == 1) {
+                return mid;
+            } else if (midN == 0) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
         }
         return -1;
     }
