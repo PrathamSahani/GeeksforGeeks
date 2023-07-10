c2e032fa970914dff0f4b20575515c090c715ec6
PRATHAM SAHANI <106865923+PrathamSahani@users.noreply.github.com>
2023-07-10 22:32:30 +0530

    Updated Solution - GfG to GitHub

--- Basic/Find triplets with zero sum/find-triplets-with-zero-sum.java
+++ Basic/Find triplets with zero sum/find-triplets-with-zero-sum.java
@@ -34,17 +34,18 @@ class Solution
     {
         //add code here.
         Arrays.sort(arr);
-        for(int i=0; i<arr.length; i++){
-            int j= i+1, k=arr.length-1;
-          while(j<k){
-              if(arr[i]+arr[j]+arr[k]==0){
-                  return true;
-              }else if(arr[i]+arr[j]+arr[k]>0){
-                  k--;
-              }else{
-                  j++;
-              }
-          }
+        for(int i=0; i<n; i++){
+            int j=i+1, k=n-1;
+            while(j<k){
+                int sum = arr[i]+arr[j]+arr[k];
+                if(sum==0){
+                    return true;
+                }else if(sum<0){
+                    j++;
+                }else{
+                    k--;
+                }
+            }
         }
         return false;
     }
