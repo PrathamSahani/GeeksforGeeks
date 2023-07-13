eb8b7623c735898859bc04352e66aeab29de7ab9
PRATHAM SAHANI <106865923+PrathamSahani@users.noreply.github.com>
2023-07-13 08:35:52 +0530

    Added solution - LeetHub

--- Number of Triangles - GFG/number-of-triangles.java
+++ Number of Triangles - GFG/number-of-triangles.java
@@ -69,17 +69,18 @@ class GFG {
 class Solution {
     public static int numberOfTriangles(int n, int[][] g) {
         // code here
-      int count =0;
-      for(int i=0; i<n; i++){
-          for(int j=0; j<n; j++){
-              for(int k=0; k<n; k++){
-                  if(i!=j && j!=k && k!=i && g[i][j]==1 && g[j][k]==1 && g[k][i]==1){
-                      count++;
-                  }
-              }
-          }
-      }
-      return count/3;
+        
+        int count =0;
+        for(int i=0; i<n; i++){
+            for(int j=0; j<n; j++){
+                for(int k=0; k<n; k++){
+                    if(i!=j && j!=k && k!=i && g[i][j]==1 && g[j][k]==1 && g[k][i]==1){
+                        count++;
+                    }
+                }
+            }
+        }
+        return count/3;
     }
 }
         
