8469c5ec73a33b5e12d419a66d777e76cba2e4b7
PRATHAM SAHANI <106865923+PrathamSahani@users.noreply.github.com>
2023-07-16 15:58:59 +0530

    Added solution - LeetHub

--- Top View of Binary Tree - GFG/top-view-of-binary-tree.java
+++ Top View of Binary Tree - GFG/top-view-of-binary-tree.java
@@ -123,16 +123,14 @@ class Node{
     }
 }
 */
- class Pair{
+class Pair{
     Node node;
     int hd;
     Pair(Node n, int h){
         node = n;
-        hd = h;
+        hd =h;
     }
-
 }
-
 class Solution
 {
     //Function to return a list of nodes visible from the top view 
@@ -140,34 +138,34 @@ class Solution
     static ArrayList<Integer> topView(Node root)
     {
         // add your code
-     
-        // ==========================
-        ArrayList<Integer> arr= new ArrayList<>();
-        if(root==null)return arr;
+        ArrayList<Integer> list = new ArrayList<>();
+        if(root==null)return list;
+        Queue<Pair> q = new LinkedList<>();
         Map<Integer, Integer> m = new TreeMap<Integer, Integer>();
-        Queue<Pair> q = new LinkedList<Pair>();
         q.add(new Pair(root, 0));
         while(!q.isEmpty()){
-            Pair it = q.poll();
+            Pair it= q.remove();
             int hd = it.hd;
-            Node temp = it.node;
+            Node temp =  it.node;
             if(m.get(hd)==null){
                 m.put(hd, temp.data);
             }
             if(temp.left!=null){
-                q.add(new Pair(temp.left, hd-1));
-            }
-            if(temp.right!=null){
+                q.add(new Pair(temp.left ,hd-1));
+            }if(temp.right!=null){
                 q.add(new Pair(temp.right, hd+1));
             }
         }
-        
-        for(Map.Entry<Integer, Integer> entry:m.entrySet()){
-            arr.add(entry.getValue());
+        for(Map.Entry<Integer, Integer> entry: m.entrySet()){
+            list.add(entry.getValue());
         }
-        return arr;
+        return list;
     }
 }
+
+
+
+
 
 
 
