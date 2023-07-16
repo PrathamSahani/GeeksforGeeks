faab26c1d7fa77dbd8bf7e75b04dcadf8d80be78
PRATHAM SAHANI <106865923+PrathamSahani@users.noreply.github.com>
2023-07-16 13:24:14 +0530

    Updated Solution - GfG to GitHub

--- Easy/Check for BST/check-for-bst.java
+++ Easy/Check for BST/check-for-bst.java
@@ -115,22 +115,15 @@ class GfG {
 
 class Solution
 {
-    //Function to check whether a Binary Tree is BST or not.
-    boolean isBST(Node root, int min, int max){
-        if(root==null)return true;
-        if(root.data>min && root.data<max){
-            boolean left = isBST(root.left, min, root.data);
-            boolean right = isBST(root.right, root.data, max);
-            return left && right;
-        }else{
-            return false;
-        }
+    boolean  f(Node root, int min, int max){
+        if(root==null)return  true;
+        if(root.data<=min || root.data>=max)return false;
+        return f(root.left, min, root.data) && f(root.right, root.data, max);
     }
+    //Function to check whether a Binary Tree is BST or not.
     boolean isBST(Node root)
     {
         // code here.
-        int min = Integer.MIN_VALUE;
-        int max = Integer.MAX_VALUE;
-        return isBST(root, min, max);
-        }
+        return f(root, Integer.MIN_VALUE, Integer.MAX_VALUE);
+    }
 }
