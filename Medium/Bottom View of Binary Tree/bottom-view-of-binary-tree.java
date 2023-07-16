d990ea4a80ad9bf7165d6ed945b6d90ea6c3532b
PRATHAM SAHANI <106865923+PrathamSahani@users.noreply.github.com>
2023-07-16 16:05:57 +0530

    Updated Solution - GfG to GitHub

--- Medium/Bottom View of Binary Tree/bottom-view-of-binary-tree.java
+++ Medium/Bottom View of Binary Tree/bottom-view-of-binary-tree.java
@@ -123,28 +123,28 @@ class Solution
     public ArrayList <Integer> bottomView(Node root)
     {
         // Code here
-        ArrayList<Integer> ans = new ArrayList<>();
-        if(root==null)return ans;
-        Map<Integer, Integer> map = new TreeMap<>();
+        ArrayList<Integer> list = new ArrayList<>();
+        Map<Integer , Integer> m = new TreeMap<Integer, Integer>();
+        if(root==null)return list;
         Queue<Node> q = new LinkedList<>();
-        
         root.hd =0;
         q.add(root);
         while(!q.isEmpty()){
-            Node temp = q.remove();
+            Node temp= q.poll();
             int hd = temp.hd;
-            map.put(hd, temp.data);
+            m.put(hd, temp.data);
             if(temp.left!=null){
                 temp.left.hd = hd-1;
                 q.add(temp.left);
-            }if(temp.right!=null){
+            }
+            if(temp.right!=null){
                 temp.right.hd = hd+1;
                 q.add(temp.right);
             }
         }
-        for(Map.Entry<Integer, Integer> entry : map.entrySet()){
-            ans.add(entry.getValue());
+        for(Map.Entry<Integer, Integer> entry: m.entrySet()){
+            list.add(entry.getValue());
         }
-        return ans;
+        return list;
     }
 }
