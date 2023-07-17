4fd73e337fb5f16e3e18b002891190e168233f8d
PRATHAM SAHANI <106865923+PrathamSahani@users.noreply.github.com>
2023-07-17 15:48:13 +0530

    Added solution - LeetHub

--- Distance from the Source (Bellman-Ford Algorithm) - GFG/distance-from-the-source-bellman-ford-algorithm.java
+++ Distance from the Source (Bellman-Ford Algorithm) - GFG/distance-from-the-source-bellman-ford-algorithm.java
@@ -53,11 +53,11 @@ class DriverClass {
 class Solution {
     static int[] bellman_ford(int V, ArrayList<ArrayList<Integer>> edges, int S) {
         // Write your code here
-      
         
         int[] dist = new int[V];
-        for(int i=0; i<V; i++)dist[i]=(int)(1e8);
-        dist[S]=0;
+        for(int i=0; i<V ; i++)dist[i]=(int)(1e8);
+        dist[S] =0;
+        
         for(int i=0; i<V-1; i++){
             for(ArrayList<Integer> it: edges){
                 int u = it.get(0);
@@ -66,7 +66,6 @@ class Solution {
                 if(dist[u]!=1e8 && dist[u]+wt<dist[v]){
                     dist[v] = dist[u]+wt;
                 }
-                
             }
         }
         for(ArrayList<Integer> it: edges){
@@ -78,9 +77,8 @@ class Solution {
                 temp[0] =-1;
                 return temp;
             }
+            
         }
         return dist;
-        
-       
     }
 }
