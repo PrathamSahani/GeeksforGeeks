744c596735fb3942afe7b0d00bc92d16da3ed28f
PRATHAM SAHANI <106865923+PrathamSahani@users.noreply.github.com>
2023-07-19 00:19:24 +0530

    Updated Solution - GfG to GitHub

--- Medium/Longest Palindromic Subsequence/longest-palindromic-subsequence.java
+++ Medium/Longest Palindromic Subsequence/longest-palindromic-subsequence.java
@@ -29,22 +29,20 @@ class Solution
     public int longestPalinSubseq(String S)
     {
         //code here
-       String p = new StringBuilder(S).reverse().toString();
-       int m = S.length();
-       int dp1[] = new int[m+1];
-       int dp2[] = new int[m+1];
-       for(int i=1; i<=m; i++){
-           for(int j=1; j<=m; j++){
-               if(S.charAt(i-1)==p.charAt(j-1)){
-                   dp1[j] = dp2[j-1]+1;
-               }else{
-                   dp1[j] = Math.max(dp1[j-1], dp2[j]);
-               }
-           }
-           for(int j=1; j<=m; j++){
-               dp2[j] = dp1[j];
-           }
-       }
-       return dp1[m];
+        String p = new StringBuilder(S).reverse().toString();
+        int n = S.length();
+        int dp[][] = new int[n+1][n+1];
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=n; j++){
+                if(S.charAt(i-1)==p.charAt(j-1)){
+                    dp[i][j] = dp[i-1][j-1]+1;
+                }
+                else{
+                    dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]);
+                }
+            }
+            
+        }
+        return dp[n][n];
     }
 }
