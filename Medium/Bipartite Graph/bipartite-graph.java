3b5c80749e19dd18b40698de2e4ac2442fa63089
PRATHAM SAHANI <106865923+PrathamSahani@users.noreply.github.com>
2023-07-18 09:25:37 +0530

    Updated Solution - GfG to GitHub

--- Medium/Bipartite Graph/bipartite-graph.java
+++ Medium/Bipartite Graph/bipartite-graph.java
@@ -37,53 +37,31 @@ class GFG
 
 class Solution
 {
-    public boolean check(int start, int V, ArrayList<ArrayList<Integer>> adj, int color[]){
-        Queue<Integer> q = new LinkedList<Integer>();
-        q.add(start);
-        color[start] =0;
-        while(!q.isEmpty()){
-            int node = q.peek();
-            q.remove();
-            for(int it: adj.get(node)){
-                if(color[it]== -1){
-                    color[it]=1-color[node];
-                    q.add(it);
-                }
-                else if(color[it]==color[node]){
-                    return false;
-                }
+    public boolean f(int node, int col, int color[], ArrayList<ArrayList<Integer>> adj){
+        color[node] = col;
+        for(int it: adj.get(node)){
+            if(color[it]==-1)
+            {
+                if(f(it, 1-col, color, adj)==false)
+                return false;
+            }else if(color[it]==col){
+                return false;
             }
         }
         return true;
+        
     }
     public boolean isBipartite(int V, ArrayList<ArrayList<Integer>>adj)
     {
         // Code here
         int color[] = new int[V];
-        for(int i=0; i<V; i++)color[i] = -1;
+        for(int i=0; i<V; i++)
+        color[i] =-1;
         for(int i=0; i<V; i++){
-            if(color[i]==-1)
-            {
-                if(check(i, V, adj, color)==false){
-                    return false;
-                }
+            if(color[i]==-1){
+                if(f(i, 0, color, adj)==false)return false;
             }
-            
         }
         return true;
     }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
