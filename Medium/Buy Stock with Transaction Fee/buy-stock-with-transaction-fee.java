b4a0ed9e06c71f6d669269ac12736f01bf25ed02
PRATHAM SAHANI <106865923+PrathamSahani@users.noreply.github.com>
2023-07-18 10:21:15 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Buy Stock with Transaction Fee/buy-stock-with-transaction-fee.java
@@ -0,0 +1,42 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class GFG{
+    public static void main(String args[]) throws IOException{
+        Scanner sc=new Scanner(System.in);
+        int t=sc.nextInt();
+        while(t-->0){
+            int N=sc.nextInt();
+            long []prices=new long[N];
+            for(int i=0;i<N;i++){
+                prices[i]=sc.nextLong();
+            }
+            int fee=sc.nextInt();
+            Solution obj=new Solution();
+            long res=obj.maximumProfit(prices,N,fee);
+            System.out.println(res);
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution{
+    public long maximumProfit(long prices[], int N, int fee){
+        // Code here]
+        if(N<=1)return 0;
+        long cash = 0;
+        long hold = cash-prices[0];
+        for(int i=1; i<prices.length; i++){
+            cash = Math.max(cash, hold+prices[i]-fee);
+            hold = Math.max(hold, cash -prices[i]);
+        }
+        return cash;
+    }
+}
