35dae5358e7eed7d9a0ffc6544d7817394db46c7
PRATHAM SAHANI <106865923+PrathamSahani@users.noreply.github.com>
2023-07-23 22:33:50 +0530

    Updated Solution - GfG to GitHub

--- Medium/Subset Sum Problem/subset-sum-problem.java
+++ Medium/Subset Sum Problem/subset-sum-problem.java
@@ -34,27 +34,26 @@ class GFG
 //User function Template for Java
 
 class Solution{
-    static boolean f(int arr[], int ind, int target, int[][] dp){
-        if(target==0)return true;
-        if(ind==0)return arr[0]==target;
-        if(dp[ind][target]!=-1)return dp[ind][target]==0?false:true;
-         
-         boolean notake = f(arr, ind-1, target , dp);
-         boolean take = false;
-         if(target>=arr[ind]){
-             take = f(arr, ind-1, target-arr[ind], dp);
-         }
-         dp[ind][target] =notake||take?1:0;
-         return notake||take;
+    
+   static int f(int i, int[] arr, int sum, int[][] dp) {
+        if (sum == 0) return 1;
+        if (i == 0) return (arr[0] == sum) ? 1 : 0;
+        if (dp[i][sum] != -1) return dp[i][sum];
+
+        int ntake = f(i - 1, arr, sum, dp);
+        int take = 0;
+        if (sum >= arr[i]) {
+            take = f(i - 1, arr, sum - arr[i], dp);
+        }
+        return dp[i][sum] = (ntake | take); // Use | instead of || for int values (1 for true, 0 for false)
     }
-
-
-    static Boolean isSubsetSum(int N, int arr[], int sum){
+    static Boolean isSubsetSum(int n, int arr[], int sum){
         // code here
-        int dp[][] = new int[N][sum+1];
-        for(int row[]: dp){
+        // int n = arr.length;
+        int[][] dp = new int[n][sum + 1];
+        for (int[] row : dp) {
             Arrays.fill(row, -1);
         }
-        return f(arr, N-1, sum , dp);
+        return f(n - 1, arr, sum, dp) == 1; 
     }
 }
