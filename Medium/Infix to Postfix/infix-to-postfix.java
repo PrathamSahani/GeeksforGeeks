efa6d1fbc75f58176acca30ab5f07bc20da57ae6
PRATHAM SAHANI <106865923+PrathamSahani@users.noreply.github.com>
2023-07-24 22:40:05 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Infix to Postfix/infix-to-postfix.java
@@ -0,0 +1,64 @@
+//{ Driver Code Starts
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class GFG {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine().trim());
+        while (t-- > 0) {
+            System.out.println(
+                new Solution().infixToPostfix(br.readLine().trim()));
+        }
+    }
+}
+// } Driver Code Ends
+
+
+class Solution {
+    // Function to convert an infix expression to a postfix expression.
+    public static int Prec(char c){
+        if(c=='^')
+        return 3;
+        else if(c=='*' || c=='/')
+        return 2;
+        else if(c=='-' || c=='+')
+        return 1;
+        else
+        return 0;
+    }
+    public static String infixToPostfix(String exp) {
+        // Your code here
+        String res ="";
+        Deque<Character> st = new ArrayDeque<>();
+        for(int i=0; i<exp.length(); i++){
+            char c = exp.charAt(i);
+            
+            if(Character.isLetterOrDigit(c))
+              res+=c;
+            else if(c=='(')
+            st.push(c);
+            else if(c==')'){
+                while(!st.isEmpty() && st.peek()!='('){
+                    res+=st.peek();
+                    st.pop();
+                }st.pop();
+            }else{
+                while(!st.isEmpty() && Prec(c)<=Prec(st.peek())){
+                    res+=st.peek();
+                    st.pop();
+                }
+                st.push(c);
+            }
+        }
+        while(!st.isEmpty()){
+            if(st.peek()=='(')return "Invalid";
+            res+=st.peek();
+            st.pop();
+        }
+        return res;
+    }
+}
