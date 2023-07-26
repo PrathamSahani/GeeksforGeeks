5482942c55478e7a4b999568679c4e8fc5e7c8c9
PRATHAM SAHANI <106865923+PrathamSahani@users.noreply.github.com>
2023-07-26 10:36:20 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Max Sum without Adjacents/max-sum-without-adjacents.java
@@ -0,0 +1,49 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+
+
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int tc = Integer.parseInt(br.readLine().trim());
+        while (tc-- > 0) {
+            String[] inputLine;
+            int n = Integer.parseInt(br.readLine().trim());
+            int[] arr = new int[n];
+            inputLine = br.readLine().trim().split(" ");
+            for (int i = 0; i < n; i++) {
+                arr[i] = Integer.parseInt(inputLine[i]);
+            }
+
+            int ans = new Solution().findMaxSum(arr, n);
+            System.out.println(ans);
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution {
+    public static int f(int ind, int arr[], int dp[]){
+        if(ind<0)return 0;
+        if(ind==0)return arr[ind];
+        if(dp[ind]!=-1)return dp[ind];
+        int pick = arr[ind]+ f(ind-2, arr, dp);
+        int np = 0+ f(ind-1, arr, dp);
+        return dp[ind]=Math.max(np, pick);
+    } 
+    int findMaxSum(int res[], int n) {
+        // code here
+        
+        int dp[] = new int[res.length];
+        Arrays.fill(dp, -1);
+        return f(res.length-1, res, dp);
+    }
+}
