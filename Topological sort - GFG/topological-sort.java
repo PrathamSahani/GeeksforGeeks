8c362af65e0cc41e53da5262fd5fd35cafc82820
PRATHAM SAHANI <106865923+PrathamSahani@users.noreply.github.com>
2023-07-26 08:52:37 +0530

    Added solution - LeetHub

--- Topological sort - GFG/topological-sort.java
+++ Topological sort - GFG/topological-sort.java
@@ -60,37 +60,33 @@ class Main {
 
 class Solution
 {
-    
     //Function to return list containing vertices in Topological order. 
     static int[] topoSort(int V, ArrayList<ArrayList<Integer>> adj) 
     {
         // add your code here
-        int ans[] = new int[V];
+        int ans[] = new int [V];
         for(int i=0; i<V; i++){
-            for(int it: adj.get(i)){
+            for(Integer it: adj.get(i)){
                 ans[it]++;
             }
         }
         Queue<Integer> q = new LinkedList<>();
         for(int i=0; i<V; i++){
-            if(ans[i]==0)
-            q.add(i);
+            if(ans[i]==0){
+                q.add(i);
+            }
         }
-        int topo[] = new int[V];
         int i=0;
+        int topo[] = new int[V];
         while(!q.isEmpty()){
             int node = q.peek();
-            q.poll();
-            topo[i++]  = node;
-            //node it in your topo sort
-            //so please remove it from the ans;
-            for(int it: adj.get(node)){
+            q.remove();
+            topo[i++] = node;
+            for(Integer it: adj.get(node)){
                 ans[it]--;
                 if(ans[it]==0)q.add(it);
             }
-            
         }
         return topo;
-        }
-        
+    }
 }
