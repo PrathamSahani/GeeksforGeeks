8bef32289da0732b2667e562559ccadf6e591582
PRATHAM SAHANI <106865923+PrathamSahani@users.noreply.github.com>
2023-07-28 18:56:17 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Merge K sorted linked lists/merge-k-sorted-linked-lists.java
@@ -0,0 +1,129 @@
+//{ Driver Code Starts
+import java.util.*;
+
+class Node
+{
+    int data;
+    Node next;
+    
+    Node(int key)
+    {
+        data = key;
+        next = null;
+    }
+}
+
+
+class GfG
+{
+    public static void printList(Node node)
+    {
+        while(node != null)
+        {
+            System.out.print(node.data + " ");
+            node = node.next;
+        }
+    }
+    
+    public static void main (String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int t = sc.nextInt();
+        while(t-- > 0)
+        {   
+            int N = sc.nextInt();
+            
+            Node []a = new Node[N];
+            
+            for(int i = 0; i < N; i++)
+            {
+                int n = sc.nextInt();
+                
+                Node head = new Node(sc.nextInt());
+                Node tail = head;
+                
+                for(int j=0; j<n-1; j++)
+                {
+                    tail.next = new Node(sc.nextInt());
+                    tail = tail.next;
+                }
+                
+                a[i] = head;
+            }
+            
+            Solution g = new Solution();
+             
+            Node res = g.mergeKList(a,N);
+            if(res!=null)
+                printList(res);
+            System.out.println();
+        }
+    }
+}
+// } Driver Code Ends
+
+
+/*class Node
+{
+    int data;
+    Node next;
+    
+    Node(int key)
+    {
+        data = key;
+        next = null;
+    }
+}
+*/
+
+// a is an array of Nodes of the heads of linked lists
+// and N is size of array a
+
+
+class Solution
+{
+    //Function to merge K sorted linked list.
+    Node mergeKList(Node[]arr,int K)
+    {
+        //Add your code here.
+        PriorityQueue<Integer> q = new PriorityQueue<>();
+        for(int i=0; i<arr.length; i++){
+            while(arr[i]!=null){
+                q.add(arr[i].data);
+                arr[i] = arr[i].next;
+            }
+        }
+        Node head = new Node(-1);
+        Node temp = head;
+        while(!q.isEmpty()){
+            Node node = new Node(q.poll());
+            temp.next = node;
+            temp= node;
+        }
+        return head.next;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
