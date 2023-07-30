17300b9108fda800278d8d24277accc04dbe6ff7
PRATHAM SAHANI <106865923+PrathamSahani@users.noreply.github.com>
2023-07-30 13:50:13 +0530

    Updated Solution - GfG to GitHub

--- Medium/Shortest path in Undirected Graph having unit distance/shortest-path-in-undirected-graph-having-unit-distance.java
+++ Medium/Shortest path in Undirected Graph having unit distance/shortest-path-in-undirected-graph-having-unit-distance.java
@@ -43,31 +43,28 @@ class Solution {
         }
         int dist[] = new int[n];
         for(int i=0; i<n; i++)
-            dist[i] = (int)1e9;
-            dist[src] =0;
-            Queue<Integer>q = new LinkedList<>();
+            dist[i] =(int)1e9;
+            dist[src]=0;
+            
+            Queue<Integer> q = new LinkedList<>();
             q.add(src);
-            while(!q.isEmpty()){
+            while(!q.isEmpty())
+            {
                 int node = q.peek();
                 q.remove();
-                for(Integer it: adj.get(node)){
-                    if(dist[node]+1<dist[it] ){
+                for(int it: adj.get(node)){
+                    if(dist[node]+1<dist[it]){
                         dist[it] = dist[node]+1;
                         q.add(it);
                     }
                 }
+                
             }
-            int ans [] = new int[n];
             for(int i=0; i<n; i++){
-                if(dist[i]!=1e9)
-                ans[i] = dist[i];
-                else 
-                ans[i] =-1;
+                if(dist[i]==1e9)
+                dist[i] =-1;
             }
-        return ans;
+            return dist;
         
-        
-       
     }
-    
 }
