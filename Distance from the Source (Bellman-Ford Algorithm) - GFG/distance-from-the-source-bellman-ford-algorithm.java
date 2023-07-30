bda7328c310f4673c16c0216626f62f366d190df
PRATHAM SAHANI <106865923+PrathamSahani@users.noreply.github.com>
2023-07-30 15:25:16 +0530

    Added solution - LeetHub

--- Distance from the Source (Bellman-Ford Algorithm) - GFG/distance-from-the-source-bellman-ford-algorithm.java
+++ Distance from the Source (Bellman-Ford Algorithm) - GFG/distance-from-the-source-bellman-ford-algorithm.java
@@ -53,31 +53,28 @@ class DriverClass {
 class Solution {
     static int[] bellman_ford(int V, ArrayList<ArrayList<Integer>> edges, int S) {
         // Write your code here
-        int[] dist = new int[V];
-        for(int i=0; i<V; i++)dist[i]= (int)(1e8);
+        int dist[] = new int[V];
+        for(int i=0; i<V; i++)
+        dist[i] = (int)1e8;
         dist[S] =0;
         for(int i=0; i<V-1; i++){
             for(ArrayList<Integer> it: edges){
                 int u = it.get(0);
                 int v = it.get(1);
-                int w = it.get(2);
-                if(dist[u]!=1e8 && dist[u]+w<dist[v]){
-                    dist[v] = dist[u]+w;
+                int wt = it.get(2);
+                if(dist[u]!=1e8 && dist[u]+wt<dist[v]){
+                    dist[v] = dist[u]+wt;
                 }
             }
         }
-        for(ArrayList<Integer> it : edges){
+        for(ArrayList<Integer> it: edges){
             int u = it.get(0);
-            int v = it.get(1);
-            int w = it.get(2);
-            if(dist[u]!=1e8 && dist[u]+w<dist[v]){
-                int temp[] = new int[1];
-                temp[0] =-1;
-                return temp;
+            int v = it.get(1) ;
+            int wt = it.get(2);
+            if(dist[u]!=1e8 && dist[u]+wt<dist[v]){
+               return new int[]{-1};
             }
-        }
-        return dist;
-        
-        
+            }
+            return dist;
     }
 }
