677377b629e118498db04578d3d2c6ac508f8eaa
PRATHAM SAHANI <106865923+PrathamSahani@users.noreply.github.com>
2023-08-01 14:17:56 +0530

    Updated Solution - GfG to GitHub

--- Medium/Minimum Spanning Tree/minimum-spanning-tree.java
+++ Medium/Minimum Spanning Tree/minimum-spanning-tree.java
@@ -31,63 +31,123 @@ public class Main{
 // } Driver Code Ends
 
 
+// User function Template for Java
 
-class Pair {
+class Pair{
     int node;
     int distance;
-    public Pair(int distance, int node) {
+    Pair(int node, int distance){
+        this.node = node;
+        this.distance = distance;
+        
+    }
+}
+
+class Solution{
+	static int spanningTree(int V, int E, int edges[][]){
+	    // Code Here. 
+	    ArrayList<ArrayList<Pair>> adj =new ArrayList<>();
+	    for(int i=0; i<V; i++){
+	        adj.add(new ArrayList<>());
+	    }
+	    for(int i=0; i<E; i++){
+	        int v = edges[i][0];
+	        int u = edges[i][1];
+	        int wt = edges[i][2];
+	        adj.get(u).add(new Pair(v, wt));
+	        adj.get(v).add(new Pair(u, wt));
+	    }
+	    PriorityQueue<Pair> pq = new PriorityQueue<>((x, y)-> x.distance-y.distance);
+	    pq.add(new Pair(0, 0));
+	    int vis[] = new int[V];
+	    int sum =0;
+	    while(!pq.isEmpty()){
+	        Pair it = pq.peek();
+	       
+	        int node = it.node;
+
+// User function Template for Java
+class Pair{
+    int node;
+    int distance;
+    Pair(int distance, int node){
         this.node = node;
         this.distance = distance;
     }
 }
-
-class Solution {
-    // Function to find sum of weights of edges of the Minimum Spanning Tree.
-    static int spanningTree(int V, int E, int edges[][]) {
-        ArrayList<ArrayList<ArrayList<Integer>>> adj = new ArrayList<>();
-        for (int i = 0; i < V; i++) {
-            adj.add(new ArrayList<>());
-        }
-
-        for (int i = 0; i < E; i++) {
-            int u = edges[i][0];
-            int v = edges[i][1];
-            int weight = edges[i][2];
-
-            adj.get(u).add(new ArrayList<>(2));
-            adj.get(u).get(adj.get(u).size() - 1).add(v);
-            adj.get(u).get(adj.get(u).size() - 1).add(weight);
-
-            adj.get(v).add(new ArrayList<>(2));
-            adj.get(v).get(adj.get(v).size() - 1).add(u);
-            adj.get(v).get(adj.get(v).size() - 1).add(weight);
-        }
-
-        PriorityQueue<Pair> pq =
-                new PriorityQueue<Pair>((x, y) -> x.distance - y.distance);
-
-        int[] vis = new int[V];
-        // {wt, node}
-        pq.add(new Pair(0, 0));
-        int sum = 0;
-        while (pq.size() > 0) {
-            int wt = pq.peek().distance;
-            int node = pq.peek().node;
-            pq.remove();
-
-            if (vis[node] == 1) continue;
-            // add it to the mst
-            vis[node] = 1;
-            sum += wt;
-
-            for (int i = 0; i < adj.get(node).size(); i++) {
-                int edW = adj.get(node).get(i).get(1);
-                int adjNode = adj.get(node).get(i).get(0);
-                if (vis[adjNode] == 0) {
-                    pq.add(new Pair(edW, adjNode));
-                }
-            }
-        }
-        return sum;
-    }
+class Solution{
+	static int spanningTree(int V, int E, int edges[][]){
+	    // Code Here. 
+	    ArrayList<ArrayList<Pair>> adj = new ArrayList<>();
+	    for(int i=0; i<V; i++){
+	        adj.add(new ArrayList<>());
+	    }
+	    for(int i=0; i<E; i++)
+	    {
+	        int u = edges[i][0];
+	        int v  = edges[i][1];
+	        int wt = edges[i][2];
+	        adj.get(u).add(new Pair(wt, v));
+	        adj.get(v).add(new Pair(wt, u));
+	    }
+	    PriorityQueue<Pair> pq = new PriorityQueue<Pair>((x, y)-> x.distance-y.distance);
+	    int[] vis = new int[V];
+	    pq.add(new Pair(0,0));
+	    int sum =0;
+	    while(!pq.isEmpty()){
+	        Pair it = pq.peek();
+	        int wt = it.distance;
+	        int node = it.node;
+	        pq.remove();
+	        if(vis[node]==1)continue;
+	        vis[node]=1;
+	        sum+=wt;
+	        
+	        for(Pair neigh: adj.get(node)){
+	            int edw = neigh.distance;
+	            int adjNode = neigh.node;
+	            if(vis[adjNode]==0){
+	                pq.add(new Pair(edw, adjNode));
+	            }
+	        }
+	    }
+	    return sum;
+	}
 }
+	        int wt = it.distance;
+	         pq.remove();
+	        if(vis[node]==1)continue;
+	        vis[node]=1;
+	        sum+=wt;
+	        for(Pair neigh: adj.get(node)){
+	            int edW = neigh.distance;
+	            int adjNode = neigh.node;
+	            if(vis[adjNode]==0){
+	                pq.add(new Pair(edW, adjNode));
+	            }
+	        }
+	    }
+	    return sum;
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
