de1dfe0e36f87741354a9cba57c4e8403a1c9ecc
PRATHAM SAHANI <106865923+PrathamSahani@users.noreply.github.com>
2023-08-01 21:58:06 +0530

    Added solution - LeetHub

--- Find the number of islands - GFG/find-the-number-of-islands.java
+++ Find the number of islands - GFG/find-the-number-of-islands.java
@@ -26,60 +26,32 @@ class GFG {
 }
 // } Driver Code Ends
 
-class Pair{
-    int first;
-    int second;
-    Pair(int first, int second){
-        this.first = first;
-        this.second = second;
-    }
-}
 class Solution {
-    
-    private void bfs(int ro, int co, int[][] vis, char[][] grid) {
-      vis[ro][co] = 1; 
-      Queue<Pair> q = new LinkedList<Pair>();
-      q.add(new Pair(ro, co)); 
-      int n = grid.length; 
-      int m = grid[0].length; 
-      
-      // until the queue becomes empty
-      while(!q.isEmpty()) {
-          int row = q.peek().first; 
-          int col = q.peek().second; 
-          q.remove(); 
-          
-          // traverse in the neighbours and mark them if its a land 
-          for(int i = -1; i<=1; i++) {
-              for(int j = -1; j <= 1; j++) {
-                  int nrow = row + i; 
-                  int ncol = col + j; 
-          // check if neighbour row and column is valid, and is an unvisited land
-                  if(nrow >= 0 && nrow < n && ncol >= 0 && ncol < m 
-                  && grid[nrow][ncol] == '1' && vis[nrow][ncol] == 0) {
-                      vis[nrow][ncol] = 1; 
-                      q.add(new Pair(nrow, ncol)); 
-                  }
-              }
-          }
-      }
-  }
-    // Function to find the number of islands.
     public int numIslands(char[][] grid) {
-        // Code here
-           int n = grid.length; 
-        int m = grid[0].length; 
-        int[][] vis = new int[n][m];
-        int cnt = 0; 
-        for(int i = 0; i < n ; i++) {
-            for(int j= 0; j < m ;j++) {
-                // if not visited and is a land
-                if(vis[i][j] == 0 && grid[i][j] == '1') {
-                    cnt++; 
-                    bfs(i, j, vis, grid); 
+        int count = 0;
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                if (grid[i][j] == '1') {
+                    count++;
+                    dfs(grid, i, j);
                 }
             }
         }
-        return cnt; 
+        return count;
+    }
+
+    public void dfs(char[][] grid, int i, int j) {
+        if (i >= 0 && j >= 0 && i < grid.length && j < grid[0].length && grid[i][j] == '1') {
+            grid[i][j] = '0'; // Mark the current land as visited
+            // Explore all 8 adjacent lands in DFS manner
+            dfs(grid, i + 1, j);
+            dfs(grid, i - 1, j);
+            dfs(grid, i, j + 1);
+            dfs(grid, i, j - 1);
+            dfs(grid, i + 1, j + 1);
+            dfs(grid, i - 1, j - 1);
+            dfs(grid, i + 1, j - 1);
+            dfs(grid, i - 1, j + 1);
+        }
     }
 }
