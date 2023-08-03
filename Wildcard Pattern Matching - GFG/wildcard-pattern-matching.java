0af2aa839a5e7c35c314625f03d9793100fc3e6a
PRATHAM SAHANI <106865923+PrathamSahani@users.noreply.github.com>
2023-08-03 13:48:38 +0530

    Added solution - LeetHub

--- Wildcard Pattern Matching - GFG/wildcard-pattern-matching.java
+++ Wildcard Pattern Matching - GFG/wildcard-pattern-matching.java
@@ -23,36 +23,39 @@ class WildcardPattern
 
 class Solution
 {
-    static boolean f(String s, int i){
-        for(int j=1; j<=i; j++){
-            if(s.charAt(j-1)!='*')return false;
-        }
-        return true;
-    }
-    int wildCard(String s1, String s2)
+
+    int wildCard(String pattern, String str)
     {
         // Your code goes here
-        int n = s1.length(), m = s2.length();
-         boolean dp[][] = new boolean[n+1][m+1];
-         dp[0][0] = true;
-         for(int i=1; i<=n; i++){
-            //  dp[i][0] = false;
-            dp[i][0] = f(s1, i);
-         }
-         for(int j=1; j<=m; j++){
-            //  dp[0][j] = s2.charAt(j-1)=='*' && dp[0][j-1];
-            dp[0][j] = false;
-         }
-         for(int i=1; i<=n; i++){
-             for(int j=1; j<=m; j++){
-                 if(s1.charAt(i-1)==s2.charAt(j-1) || s1.charAt(i-1)=='?')
-                 dp[i][j] = dp[i-1][j-1];
-                 else if(s1.charAt(i-1)=='*')
-                 dp[i][j] = dp[i-1][j] || dp[i][j-1];
-                 else 
-                 dp[i][j] = false;
-             }
-         }
-         return dp[n][m]?1:0;
+        int n=str.length();
+        int m=pattern.length();
+        int ans=0;
+        boolean dp[][]=new boolean[n+1][m+1];
+        dp[0][0]=true;
+        for(int i=1;i<n+1;i++){
+            dp[i][0]=false;
+        }
+        for(int j=1;j<m+1;j++){
+            if(pattern.charAt(j-1)=='*'){
+                dp[0][j]=dp[0][j-1];
+            }
+        }
+        for(int i=1;i<n+1;i++){
+            for(int j=1;j<m+1;j++){
+                if(str.charAt(i-1)==pattern.charAt(j-1)||pattern.charAt(j-1)=='?'){
+                    dp[i][j]=dp[i-1][j-1];
+                }
+                else if(pattern.charAt(j-1)=='*'){
+                    dp[i][j]=dp[i][j-1]||dp[i-1][j];
+                }
+                else{
+                    dp[i][j]=false;
+                }
+            }
+        }
+        if(dp[n][m]==true){
+            ans=1;
+        }
+        return ans;
     }
 }
