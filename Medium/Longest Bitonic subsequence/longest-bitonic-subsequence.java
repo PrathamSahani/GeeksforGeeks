aede01b20981be1bf6e506a34519ccb6f41c5c5b
PRATHAM SAHANI <106865923+PrathamSahani@users.noreply.github.com>
2023-08-03 21:47:45 +0530

    Updated Solution - GfG to GitHub

--- Medium/Longest Bitonic subsequence/longest-bitonic-subsequence.java
+++ Medium/Longest Bitonic subsequence/longest-bitonic-subsequence.java
@@ -35,38 +35,32 @@ class Solution
     public int LongestBitonicSequence(int[] arr)
     {
         // Code here
-      int n = arr.length;
-    int[] dp1= new int[n];
-    int[] dp2= new int[n];
-    
-    Arrays.fill(dp1,1);
-    Arrays.fill(dp2,1);
-    
-    for(int i=0; i<=n-1; i++){
-        for(int prev_index = 0; prev_index <=i-1; prev_index ++){
-            
-            if(arr[prev_index]<arr[i]){
-                dp1[i] = Math.max(dp1[i], 1 + dp1[prev_index]);
+        int n = arr.length;
+        int[] dp1 = new int[n];
+        int[] dp2 = new int[n];
+        Arrays.fill(dp1, 1);
+        Arrays.fill(dp2, 1);
+        for(int i=0; i<n; i++){
+            for(int j=0; j<i; j++){
+                if(arr[j]<arr[i])
+                dp1[i] = Math.max(dp1[i], 1+dp1[j]);
             }
         }
-    }
-    
-    // reverse the direction of nested loops
-    for(int i=n-1; i>=0; i--){
-        for(int prev_index = n-1; prev_index >i; prev_index --){
-            
-            if(arr[prev_index]<arr[i]){
-                dp2[i] = Math.max(dp2[i], 1 + dp2[prev_index]);
+        for(int i=n-1; i>=0; i--){
+            for(int j=n-1; j>i; j--){
+                if(arr[j]<arr[i]){
+                    dp2[i] = Math.max(dp2[i], 1+dp2[j]);
+                }
             }
+            
         }
+        int maxi =-1;
+        for(int i=0; i<n; i++){
+            maxi = Math.max(maxi, dp1[i]+dp2[i]-1);
+        }
+        return maxi;
+    
+        
     }
     
-    int maxi = -1;
-    
-    for(int i=0; i<n; i++){
-        maxi = Math.max(maxi, dp1[i] + dp2[i] - 1);
-    }
-    
-    return maxi;
-    }
 }
