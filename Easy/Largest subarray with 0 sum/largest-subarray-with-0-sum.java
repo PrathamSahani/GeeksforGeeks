f365bd6ede798ccdd2a21c561cb82a8dadce8966
PRATHAM SAHANI <106865923+PrathamSahani@users.noreply.github.com>
2023-08-04 09:40:15 +0530

    Updated Solution - GfG to GitHub

--- Easy/Largest subarray with 0 sum/largest-subarray-with-0-sum.java
+++ Easy/Largest subarray with 0 sum/largest-subarray-with-0-sum.java
@@ -32,9 +32,8 @@ class GfG
     int maxLen(int arr[], int n)
     {
         // Your code here
-        int max=0;
+        int sum =0;int max =0;
         HashMap<Integer, Integer> map = new HashMap<>();
-        int sum =0;
         for(int i=0; i<n; i++){
             sum+=arr[i];
             if(sum==0){
@@ -46,7 +45,6 @@ class GfG
             else{
                 map.put(sum, i);
             }
-        }
-        return max;
+        }return max;
     }
 }
