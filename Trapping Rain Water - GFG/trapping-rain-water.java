6cdb6459f76f9a53b23f265b3a937e2420e587b4
PRATHAM SAHANI <106865923+PrathamSahani@users.noreply.github.com>
2023-08-05 17:58:09 +0530

    Added solution - LeetHub

--- Trapping Rain Water - GFG/trapping-rain-water.java
+++ Trapping Rain Water - GFG/trapping-rain-water.java
@@ -40,21 +40,21 @@ class Solution{
     // Function to find the trapped water between the blocks.
     static long trappingWater(int arr[], int n) { 
         // Your code here
-        long left[] = new long[n];
-        long right[] = new long[n];
-        left[0] = arr[0];
-        for(int i=1; i<n; i++){
-            left[i] = Math.max(left[i-1], arr[i]);
-        }
-        right[n-1] = arr[n-1];
-        for(int i=n-2; i>=0; i--){
-            right[i] = Math.max(right[i+1], arr[i]);
-        }
-        long ans=0;
-        for(int i=0; i<n; i++){
-            ans+=Math.min(left[i], right[i])-arr[i];
-        }
-        return ans;
+         long left[] =new long[n];
+         long right[] = new long[n];
+         left[0] = arr[0];
+         for(int i=1; i<n; i++){
+             left[i] = Math.max(left[i-1], arr[i]);
+         }
+         right[n-1] = arr[n-1];
+         for(int i=n-2; i>=0; i--){
+             right[i] = Math.max(right[i+1], arr[i]);
+         }
+         long ans=0;
+         for(int i=0; i<n; i++){
+             ans+=Math.min(left[i], right[i])-arr[i];
+         }
+         return ans;
     } 
 }
 
