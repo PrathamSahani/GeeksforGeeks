065af72de25d4babad81af8cdfbdf90d6ade6aaf
PRATHAM SAHANI <106865923+PrathamSahani@users.noreply.github.com>
2023-08-07 00:40:53 +0530

    Updated Solution - GfG to GitHub

--- Easy/Find Nth root of M/find-nth-root-of-m.java
+++ Easy/Find Nth root of M/find-nth-root-of-m.java
@@ -35,24 +35,22 @@ class Solution
         for(int i=1; i<=n; i++){
             ans = ans*mid;
             if(ans>m)return 2;
-        }
-        if(ans==m)return 1;
+        }if(ans==m)return 1;
+        else
         return 0;
     }
     public int NthRoot(int n, int m)
     {
         // code here
-        int low =1, high=m;
+        int low =1, high = m;
         while(low<=high){
             int mid = (low+high)/2;
-            int ans = f(mid, n , m);
-            if(ans==1){
-                return mid;
-            }else if(ans==0){
-                low = mid+1;
-            }else{
-                high = mid-1;
-            }
+            int ans = f(mid, n, m);
+            if(ans==1)return mid;
+            else if(ans==0)
+            low = mid+1;
+            else 
+            high = mid-1;
         }
         return -1;
     }
