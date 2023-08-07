aa40c3791f898d91212770588a933660eaef492e
PRATHAM SAHANI <106865923+PrathamSahani@users.noreply.github.com>
2023-08-07 09:19:10 +0530

    Added solution - LeetHub

--- Capacity To Ship Packages Within D Days - GFG/capacity-to-ship-packages-within-d-days.java
+++ Capacity To Ship Packages Within D Days - GFG/capacity-to-ship-packages-within-d-days.java
@@ -29,37 +29,37 @@ class GFG {
 //User function Template for Java
 
 class Solution {
-    public static boolean solve(int arr[], int max, int d){
-        int weight =0, day=1;
+    public static int f(int arr[], int d){
+        int day =1;
+        int ans =0;
         for(int i=0; i<arr.length; i++){
-            if(weight+arr[i]>max){
+            if(ans+arr[i]<=d){
+                ans+= arr[i];
+            }else{
                 day++;
-                weight = arr[i];
-            }else
-            weight+=arr[i];
+                ans=arr[i];
+            }
         }
-        return day<=d;
+        return day;
     }
     static int leastWeightCapacity(int[] arr, int N, int D) {
         // code here
-        int max =0; int sum =0;
-        for(int i=0; i<arr.length; i++){
-            max = Math.max(arr[i], max);
-                sum+=arr[i];
+        int low =0;
+        int high =0;
+        for(int i=0; i<N; i++){
+            high+=arr[i];
+            low= Math.max(low, arr[i]);
+        }
+        while(low<=high){
+            int mid = (low+high)/2;
+            int ans = f(arr, mid);
+            if(ans<=D){
+                high = mid-1;
+            }else{
+                low = mid+1;
             }
-            int lo = max;int ans =-1;
-            int hi =  sum;
-            if(D==arr.length)return max;
-            while(lo<=hi){
-                int mid = lo+(hi-lo)/2;
-                if(solve(arr, mid, D)){
-                    ans = mid;
-                    hi = mid-1;
-                }
-                else
-                lo = mid+1;
             
-        }return ans;
-       
+        }
+        return low;
     }
 };
