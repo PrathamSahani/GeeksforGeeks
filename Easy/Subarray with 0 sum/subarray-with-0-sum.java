73e96227ce00fa795cf49fe1abc0c48b8b61e995
PRATHAM SAHANI <106865923+PrathamSahani@users.noreply.github.com>
2023-08-08 18:14:09 +0530

    Updated Solution - GfG to GitHub

--- Easy/Subarray with 0 sum/subarray-with-0-sum.java
+++ Easy/Subarray with 0 sum/subarray-with-0-sum.java
@@ -40,20 +40,22 @@ class GFG {
 // } Driver Code Ends
 
 
+
 class Solution{
     //Function to check whether there is a subarray present with 0-sum or not.
     static boolean findsum(int arr[],int n)
     {
         //Your code here
-        int sum=0;
-         HashSet<Integer> set = new HashSet<>();
+         HashMap<Integer, Integer> map = new HashMap<>();
+         map.put(0, 1);
+         int sum =0;
          for(int i=0; i<n; i++){
              sum+=arr[i];
-             if(arr[i]==0 || sum==0 || set.contains(sum)){
-                    return true;
-                
+             if(map.containsKey(sum)){
+                 return true;
+             }else{
+                 map.put(sum, 1);
              }
-           set.add(sum);
          }
          return false;
     }
