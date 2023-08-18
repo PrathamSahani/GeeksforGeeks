75c94db6b78887fc80f5264810d576c5e1a8a280
PRATHAM SAHANI <106865923+PrathamSahani@users.noreply.github.com>
2023-08-18 19:00:27 +0530

    Added solution - LeetHub

--- Peak element - GFG/peak-element.java
+++ Peak element - GFG/peak-element.java
@@ -58,12 +58,14 @@ class Solution
        int low = 1, high = n-2;
        while(low<=high){
            int mid = (low+high)/2;
-           if(arr[mid]>arr[mid-1] && arr[mid]>=arr[mid+1]){
-               return mid; 
+           if(arr[mid]>arr[mid-1] && arr[mid]>arr[mid+1]){
+               return mid;
+           }else if(arr[mid]<arr[mid+1]){
+               low = mid+1;
+           }else{
+               high = mid-1;
            }
-           else if(arr[mid]>arr[mid-1])low = mid+1;
-           else high = mid-1;
        }
-       return 0;
+       return low;
     }
 }
