3d71bbd9af2de3290ce82d09eda9b8adcc6dfcb3
PRATHAM SAHANI <106865923+PrathamSahani@users.noreply.github.com>
2023-08-24 15:30:57 +0530

    Updated Solution - GfG to GitHub

--- Medium/Minimum Multiplications to reach End/minimum-multiplications-to-reach-end.java
+++ Medium/Minimum Multiplications to reach End/minimum-multiplications-to-reach-end.java
@@ -30,41 +30,27 @@ class GFG {
 }
 // } Driver Code Ends
 
-
-// User function Template for Java
-class Pair{
-    int first, second;
-    Pair(int first, int second){
-        this.first = first;
-        this.second = second;
-    }
-}
 class Solution {
     int minimumMultiplications(int[] arr, int start, int end) {
-        // Your code here
-        Queue<Pair> q = new LinkedList<>();
-        q.add(new Pair(start, 0));
-        int dist[] = new int[100000];
-        Arrays.fill(dist,Integer.MAX_VALUE);
-        dist[start] =0;
-        int mod = 100000;
-        int n = arr.length;
+        Queue<Integer> q = new LinkedList<>();
+        q.add(start);
+        int[] dist = new int[100000];
+        Arrays.fill(dist, Integer.MAX_VALUE);
+        dist[start] = 0;
         
-        while(!q.isEmpty()){
-            int node = q.peek().first;
-            int step = q.peek().second;
-            q.remove();
-            for(int i=0; i<n; i++){
-                int num = (int)(((long)arr[i]*node)%mod);
-                if(step+1<dist[num]){
-                    dist[num] = step+1;
-                    if(num==end)return step+1;
-                    q.add(new Pair(num, step+1));
+        while (!q.isEmpty()) {
+            int node = q.poll();
+            
+            for (int num : arr) {
+                int nextNode = (int) (((long) num * node) % 100000);
+                
+                if (dist[nextNode] > dist[node] + 1) {
+                    dist[nextNode] = dist[node] + 1;
+                    q.add(nextNode);
                 }
             }
-        }if(dist[end]==Integer.MAX_VALUE){
-            return -1;
         }
-        return dist[end];
+        
+        return dist[end] == Integer.MAX_VALUE ? -1 : dist[end];
     }
 }
