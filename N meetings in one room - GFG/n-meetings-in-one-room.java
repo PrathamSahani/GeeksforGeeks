98085275b126bdf64f215c05d5489efa1aa3c62c
PRATHAM SAHANI <106865923+PrathamSahani@users.noreply.github.com>
2023-09-11 11:32:44 +0530

    Added solution - LeetHub

--- N meetings in one room - GFG/n-meetings-in-one-room.java
+++ N meetings in one room - GFG/n-meetings-in-one-room.java
@@ -35,33 +35,30 @@ class GFG {
 
 class Solution 
 {
+    //Function to find the maximum number of meetings that can
+    //be performed in a meeting room.
     static class Pair{
         int start,end;
-        Pair(int start,int end){
-            this.start=start;
-            this.end=end;
+        Pair(int start, int end){
+            this.start = start;
+            this.end = end;
         }
     }
-    
     public static int maxMeetings(int start[], int end[], int n)
     {
-        ArrayList<Pair> list=new ArrayList<>();
-        
-        for(int i=0;i<n;i++){
-            list.add(new Pair(start[i],end[i]));       
+        // add your code here
+        ArrayList<Pair> list = new ArrayList<>();
+        for(int i=0; i<n; i++){
+            list.add(new Pair(start[i], end[i]));
         }
-        
-        Collections.sort(list,(a,b)->(a.end-b.end));
-        
-        int res=1,prev=0;
-        for(int curr=1;curr<n;curr++){
-            if(list.get(curr).start > list.get(prev).end){
+        Collections.sort(list, (a, b)->(a.end-b.end));
+        int res =1, prev =0;
+        for(int curr=1; curr<n; curr++){
+            if(list.get(curr).start> list.get(prev).end){
                 res++;
-                prev=curr;
+                prev = curr;
             }
         }
         return res;
     }
 }
-
- 
