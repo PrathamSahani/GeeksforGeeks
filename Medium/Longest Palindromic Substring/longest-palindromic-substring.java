58ef0a4369bd868aeb6aa1cc1b5ed19dbfc81fc7
PRATHAM SAHANI <106865923+PrathamSahani@users.noreply.github.com>
2023-09-29 21:41:46 +0530

    Updated Solution - GfG to GitHub

--- Medium/Longest Palindromic Substring/longest-palindromic-substring.java
+++ Medium/Longest Palindromic Substring/longest-palindromic-substring.java
@@ -25,28 +25,29 @@ class GFG
 //User function Template for Java
 
 class Solution{
-    public static boolean solve(String str){
-        int i=0; int j=str.length()-1;
-        while(i<j){
-            if(str.charAt(i)!=str.charAt(j)){
-                return false;
-            }
-            i++;j--;
-        }
-        return true;
-    }
     String longestPalindrome(String S){
         // code here
-        if(S.length()==1)return S;
-        String st="";
-        for(int i=S.length(); i>0; i--){
+        String str ="";
+        for(int i=S.length(); i>=0; i--){
             for(int j=0; j<S.length()-i+1; j++){
-                st = S.substring(j, i+j);
-                if(solve(st)){
-                    return st;
+                str = S.substring(j, i+j);
+                if(swap(str)){
+                    return str;
                 }
             }
         }
         return "";
+    }
+    public static boolean swap(String str){
+        int i=0, j= str.length()-1;
+        while(i<=j){
+            if(str.charAt(i)!=str.charAt(j)){
+                return false;
+            }else{
+                i++;
+                j--;
+            }
+        }
+        return true;
     }
 }
