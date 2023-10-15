d6774b217515f9957d93e5c7a940e903a131459e
PRATHAM SAHANI <106865923+PrathamSahani@users.noreply.github.com>
2023-10-15 13:54:21 +0530

    Updated Solution - GfG to GitHub

--- Medium/Remove loop in Linked List/remove-loop-in-linked-list.java
+++ Medium/Remove loop in Linked List/remove-loop-in-linked-list.java
@@ -96,6 +96,8 @@ class GFG
 // } Driver Code Ends
 
 
+
+
 /*
 class Node
 {
@@ -114,15 +116,12 @@ class Solution
         // remove the loop without losing any nodes
       Node slow = head;
       while(slow!=null && slow.next!=null){
-          if(slow.next.data == Integer.MAX_VALUE){
+          if(slow.next.data ==Integer.MAX_VALUE){
               slow.next = null;
               break;
           }
-          slow.data= Integer.MAX_VALUE;
-          slow = slow.next;
+          slow.data = Integer.MAX_VALUE;
+          slow= slow.next;
       }
-      
-       
-     
     }
 }
