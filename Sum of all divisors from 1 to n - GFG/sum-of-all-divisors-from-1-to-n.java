1f1f12ecdcdc34ebf2590b447a88785bf5f4fa55
PRATHAM SAHANI <106865923+PrathamSahani@users.noreply.github.com>
2023-10-21 00:10:27 +0530

    Added solution - LeetHub

---
+++ Sum of all divisors from 1 to n - GFG/sum-of-all-divisors-from-1-to-n.java
@@ -0,0 +1,39 @@
+//{ Driver Code Starts
+//Initial Template for Java
+import java.io.*;
+import java.util.*;
+
+class GFG
+{
+    public static void main(String args[])throws IOException
+    {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while(t-- > 0)
+        {
+            int N=sc.nextInt();
+            Solution ob = new Solution();
+            long ans  = ob.sumOfDivisors(N);
+            System.out.println(ans);
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+class Solution{
+    static long sumOfDivisors(int num){
+        
+        long sum =0;
+        for(int i=1; i<=num; i++){
+          
+                sum+=i*(num/i);
+            
+        }
+        return sum;
+  
+    }
+}
+
