71ab5a8632981b4ebb2b267201cb179e11db0a4c
PRATHAM SAHANI <106865923+PrathamSahani@users.noreply.github.com>
2023-11-01 01:16:59 +0530

    Updated Solution - GfG to GitHub

--- Easy/Left View of Binary Tree/left-view-of-binary-tree.java
+++ Easy/Left View of Binary Tree/left-view-of-binary-tree.java
@@ -125,31 +125,31 @@ class Tree
     //Function to return list containing elements of left view of binary tree.
     ArrayList<Integer> leftView(Node root)
     {
-      // Your code here
+      // Your code here\
+      
       ArrayList<Integer> list = new ArrayList<>();
-      if(root==null)return list;
-      Queue<Node>   q = new LinkedList<>();
-      q.add(root);int level=0;
+      if(root==null) return list;
+      Queue<Node> q = new LinkedList<>();
+      q.add(root);
       while(!q.isEmpty()){
-         
           int n = q.size();
           for(int i=0; i<n; i++){
               Node temp = q.poll();
               if(i==0){
-              list.add(temp.data);
+                  list.add(temp.data);
               }
-             if(temp.left!=null)
-             q.add(temp.left);
-             if(temp.right!=null)
-             q.add(temp.right);
+              if(temp.left!=null){
+                  q.add(temp.left);
+              }
+              if(temp.right!=null){
+                  q.add(temp.right);
+              }
           }
           
-           level++;
       }
       return list;
     }
 }
-
 
 
 
