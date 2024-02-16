c36ef460727395adf95bb0272372d72c56515005
Pratham Sahani <106865923+PrathamSahani@users.noreply.github.com>
2024-02-16 07:36:54 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Flatten BST to sorted list/flatten-bst-to-sorted-list.py
@@ -0,0 +1,105 @@
+#User function Template for python3
+
+'''
+class Node:
+    def __init__(self, val):
+        self.data = val
+        self.left = None
+        self.right = None
+'''
+import sys
+sys.setrecursionlimit(10**6)
+class Solution:
+    def flattenBST(self, root):
+        # code here
+        head=Node(0)
+        def travel(node,head):
+            if not node:
+                return head
+            left=node.left
+            right=node.right
+            node.left=node.right=None
+            travel(left,head).right=node
+            return travel(right,node)
+        travel(root,head)
+        return head.right
+
+
+
+
+
+
+#{ 
+ # Driver Code Starts
+
+#Initial Template for Python 3
+
+from queue import Queue
+
+class Node:
+    def __init__(self, val):
+        self.data = val
+        self.left = None
+        self.right = None
+
+
+def newNode(key):
+    node = Node(key)
+    return node
+
+def buildTree(s):
+    if len(s) == 0 or s[0] == 'N':
+        return None
+
+    ip = s.split()
+    root = newNode(int(ip[0]))
+    q = Queue()
+    q.put(root)
+
+    i = 1
+    while not q.empty() and i < len(ip):
+        currNode = q.get()
+
+        currVal = ip[i]
+        if currVal != "N":
+            currNode.left = newNode(int(currVal))
+            q.put(currNode.left)
+
+        i += 1
+        if i >= len(ip):
+            break
+        currVal = ip[i]
+        if currVal != "N":
+            currNode.right = newNode(int(currVal))
+            q.put(currNode.right)
+
+        i += 1
+
+    return root
+
+
+def printList(head):
+    # if head==None:
+    #     return
+    # print(head.data, end=" ")
+    # printList(head.left)
+    # printList(head.right)
+    while head:
+        if head.left:
+            print(-1,end=" ")
+        print(head.data, end=" ")
+        head = head.right
+        
+    print()
+
+if __name__ == "__main__":
+    t = int(input())
+    for _ in range(t):
+        s = input()
+        root = buildTree(s)
+        ob = Solution()
+        ans = ob.flattenBST(root)
+        printList(ans)
+        # print()
+
+# } Driver Code Ends
