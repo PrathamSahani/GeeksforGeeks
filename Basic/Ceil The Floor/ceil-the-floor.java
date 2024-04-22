dc4cf705c15c3b9105982fa6b92ddaf70f5961fb
Pratham Sahani <106865923+PrathamSahani@users.noreply.github.com>
2024-04-22 19:57:03 +0530

    Updated Solution - GfG to GitHub

--- Basic/Ceil The Floor/ceil-the-floor.java
+++ Basic/Ceil The Floor/ceil-the-floor.java
@@ -51,36 +51,26 @@ class Pair {
 
 
 class Solve {
-    static int floor(int arr[], int n, int x){
-        int low =0, high = n-1;int ans =-1;
-        while(low<=high){
-            int mid = low+(high-low)/2;
-            if(arr[mid]<=x){
-                ans = arr[mid];
-                low = mid+1;
-            }else {
-                high = mid-1;
-            }
-            
-        }
-        return ans;
-    }
-    static int ceil(int arr[], int n, int x){
-        int ans =-1, low =0 , high = n-1;
-        while(low<=high){
-            int mid = low+(high-low)/2;
-            if(arr[mid]>=x){
-                ans  = arr[mid];
-                high = mid-1;
-                }else {
-                    low = mid+1;
-                }
-            }return ans;
-    }
     Pair getFloorAndCeil(int[] arr, int n, int x) {
         // code here
+        int floor = -1, ceil =-1;
         Arrays.sort(arr);
-        return new Pair(floor(arr, n, x), ceil(arr, n, x));
+        for(int i=0; i<n; i++){
+            if(arr[i]<=x){
+                floor = arr[i];
+                
+            }
+            if(arr[i]>=x){
+                ceil = arr[i];
+                break;
+            }
+        }
+        Pair pair = new Pair(-1, -1);
+        pair.floor = floor;
+        pair.ceil = ceil;
+        return pair;
     }
 }
 
+     
+      
